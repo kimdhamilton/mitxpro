0182364969c1c3abefb3065f88f214a3beaf535b
Peter Pinch <430126+pdpinch@users.noreply.github.com>
2020-06-21 22:23:37 -0400

    clarify commit regarding Safari autoplay case

--- static/js/hero.js
+++ static/js/hero.js
@@ -58,8 +58,8 @@ function configureHlsVideo(selector, autoplay) {
     // hls.js is not supported on platforms that do not have Media Source Extensions (MSE) enabled.
     // When the browser has built-in HLS support (check using `canPlayType`), we can provide an HLS manifest (i.e. .m3u8 URL) directly to the video element throught the `src` property.
     // This is using the built-in support of the plain video element, without using hls.js.
-    // Note: it would be more normal to wait on the 'canplay' event below however on Safari (where you are most likely to find built-in HLS support) the video.src URL must be on the user-driven
-    // white-list before a 'canplay' event will be emitted; the last video event that can be reliably listened-for when the URL is not on the white-list is 'loadedmetadata'.
+    // Note: it would be more normal to wait on the 'canplay' event below however on Safari (where you are most likely to find built-in HLS support) the user must explicitly enable autoplay for the video.src domain
+    // before a 'canplay' event will be emitted; the last video event that can be reliably listened-for when autoplay is not allowed on the domain is 'loadedmetadata'.
     else if (video.canPlayType("application/vnd.apple.mpegurl")) {
       video.src = videoUrl;
       if (autoplay) {
